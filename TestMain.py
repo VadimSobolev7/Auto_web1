d877d23befa69bb64e8acdd4409bd095f19dd08a
Vadim Sobolev <vadsobolev@yandex.ru>
2024-06-12 19:59:57 +0300

    top

---
+++ TestMain.py
@@ -0,0 +1,62 @@
+import yaml
+from Module import Site
+import time
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+site = Site(testdata["address"])
+login = testdata['login']
+password = testdata['password']
+title = testdata['title']
+description = testdata['description']
+content = testdata['content']
+
+def test_step1(sel_1, sel_2, sel_3, btn_sel, code_res):
+    input1 = site.find_element("xpath", sel_1)
+    input1.send_keys("test")
+    input2 = site.find_element("xpath", sel_2)
+    input2.send_keys("test")
+    btn = site.find_element("css", btn_sel)
+    btn.click()
+    err_label = site.find_element("xpath", sel_3)
+    res = err_label.text
+    assert res == code_res
+
+def test_step2(sel_1, sel_2, result, auth, btn_sel):
+    input1 = site.find_element("xpath", sel_1)
+    input1.clear()
+    input1.send_keys(login)
+    input2 = site.find_element("xpath", sel_2)
+    input2.clear()
+    input2.send_keys(password)
+    btn = site.find_element("css", btn_sel)
+    btn.click()
+    auth = site.find_element("xpath", auth)
+    res = auth.text
+    #site.close_connection()
+    assert res == result
+
+def test_step3(btn_createpost, input_title, input_des, input_cont, btn_savepost, check_post, result1):
+    time.sleep(1)
+    btn = site.find_element("xpath", btn_createpost)
+    time.sleep(1)
+    btn.click()
+    time.sleep(1)
+    input1 = site.find_element("xpath", input_title)
+    input1.send_keys(title)
+    time.sleep(1)
+    input2 = site.find_element("xpath", input_des)
+    input2.send_keys(description)
+    time.sleep(1)
+    input3 = site.find_element("xpath", input_cont)
+    input3.send_keys(content)
+    time.sleep(1)
+    btn = site.find_element("xpath", btn_savepost)
+    time.sleep(1)
+    btn.click()
+    time.sleep(1)
+    check_post = site.find_element("xpath", check_post)
+    res = check_post.text
+    site.close_connection()
+    assert res == result1
